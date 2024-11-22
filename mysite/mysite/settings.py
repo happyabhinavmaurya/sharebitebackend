bb33039ea0f8824d436b11cb5dbb7414b16ccb2f
Abhinav Maurya <106180079+happyabhinavmaurya@users.noreply.github.com>
2024-11-22 19:06:51 +0530

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'api',
+    'mysite.api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
